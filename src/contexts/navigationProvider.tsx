c18f8f73f872e8dff8019b8ba3285855c27eb437
Guillermo <jguillermo.moranl@gmail.com>
2021-10-19 11:10:33 -0300

    Agrego tipos de navegacion

--- src/contexts/navigationProvider.tsx
+++ src/contexts/navigationProvider.tsx
@@ -1,11 +1,18 @@
 import React, { createContext, ReactChildren, FC } from "react";
+import PageOne from "../pages/stackone/pageOne";
 
 type PropsContext = {
   children: ReactChildren;
 };
 
+type NavScreenType = {
+  component: FC;
+  path: string;
+  label: string;
+};
+
 type navigationContextType = {
-  stackNavigation: [];
+  stackNavigation: NavScreenType[];
 };
 
 export const NavigationContext = createContext<navigationContextType>({
@@ -14,7 +21,27 @@ export const NavigationContext = createContext<navigationContextType>({
 
 const NavigationProvider: FC<PropsContext> = ({ children }): JSX.Element => {
   return (
-    <NavigationContext.Provider value={{ stackNavigation: [] }}>
+    <NavigationContext.Provider
+      value={{
+        stackNavigation: [
+          {
+            component: PageOne,
+            path: "/one",
+            label: "Nav to page one",
+          },
+          {
+            component: PageOne,
+            path: "/two",
+            label: "Nav to page twoF",
+          },
+          {
+            component: PageOne,
+            path: "/three",
+            label: "Nav to page three",
+          },
+        ],
+      }}
+    >
       {children}
     </NavigationContext.Provider>
   );
